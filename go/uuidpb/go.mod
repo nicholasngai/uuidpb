47a5736f2f4dd776d118486dc32e2695f7ac9c7b
Nicholas Ngai <nicholas@ngai.me>
2023-09-09 00:16:39 -0700

    Add uuid_test.go

--- go/uuidpb/go.mod
+++ go/uuidpb/go.mod
@@ -3,6 +3,14 @@ module github.com/nicholasngai/uuidpb/go/uuidpb
 go 1.20
 
 require (
+	github.com/google/go-cmp v0.5.5
 	github.com/google/uuid v1.3.1
+	github.com/stretchr/testify v1.8.4
 	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ go/uuidpb/uuid_test.go
@@ -0,0 +1,35 @@
+package uuidpb
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func assertProtoEqual(t *testing.T, expected proto.Message, actual proto.Message) {
+	assert.Truef(t, proto.Equal(expected, actual), cmp.Diff(expected, actual, protocmp.Transform()))
+}
+
+func TestNew(t *testing.T) {
+	id := uuid.MustParse("e63ebbbb-d54e-4598-8ceb-58fe1a353675")
+	idProto := New(id)
+	expected := &UUID{
+		Hi: 16590904492691441048,
+		Lo: 10154307633221547637,
+	}
+	assertProtoEqual(t, expected, idProto)
+}
+
+func TestUUID_ToUUID(t *testing.T) {
+	idProto := &UUID{
+		Hi: 16590904492691441048,
+		Lo: 10154307633221547637,
+	}
+	id := idProto.ToUUID()
+	expected := uuid.MustParse("e63ebbbb-d54e-4598-8ceb-58fe1a353675")
+	assert.Equal(t, expected, id)
+}
